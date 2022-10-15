9ecb677373d2e34f8a4ae5f995e2aa0541625b83
yuvraj1905 <36763315+mayank0801@users.noreply.github.com>
2022-10-15 12:44:26 +0530

    docs: added index.js file from repl

---
+++ index.js
@@ -0,0 +1,52 @@
+const readLineSync = require("readline-sync");
+const userName = readLineSync.question("What is your name? ");
+console.log(`Hello ${userName.toUpperCase()}!!!
+Welcome!!
+WHAT DO YOU KNOW ABOUT INDIAN CRICKET TEAM 😊`);
+
+var Score = 0;
+const helper = function (question, answer) {
+  var userAns = readLineSync.question(question);
+
+  if (userAns === answer) {
+    console.log("Right!!");
+    Score++;
+  } else {
+    console.log("Wrong answer!");
+  }
+};
+
+var questions = [
+  {
+    Question: "who is current captain of Indian cricket team? ",
+    Answer: "Rohit sharma",
+  },
+  {
+    Question: "Who led the indian team to 2011 world cup victory? ",
+    Answer: "Dhoni",
+  },
+  {
+    Question: "Who is India's most run scorer in ODIs? ",
+    Answer: "Sachin",
+  },
+  {
+    Question: "Who has hit 6 sixes in an over of T20I? ",
+    Answer: "Yuvraj singh",
+  },
+  {
+    Question: "Who is the leading run scorer in T20Is for india ?",
+    Answer: "Virat kohli",
+  },
+];
+
+for (var i = 0; i < questions.length; i++) {
+  var currentQuestion = questions[i];
+  // helper(questions[i].Question, questions[i].Answer);
+  helper(currentQuestion.Question, currentQuestion.Answer);
+}
+
+if (Score > 0) {
+  console.log(`Great!! Your score is: ${Score}`);
+} else {
+  console.log("Never mind! Your score is", Score);
+}
